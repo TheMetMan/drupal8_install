f5849bb4536492ae556935c50f60ed048b89c19f
Francis TheMetMan <themetman@themetman.net>
2017-05-23 13:38:12 +0100

    Edited Typo in config.sh

--- config.sh
+++ config.sh
@@ -2,19 +2,19 @@
 # the workingFolder is where you put the script and base_files Folder
 #
 useComposer="0"             # set to 0 to install with Composer else set to 1
-workingFolder="/home/francis/bin/drupalInstall"
-siteFolder="site3.local"	#Location of site
-apacheRoot="/var/www/html"
-apacheUser="francis"
-apacheGroup="www-data"
-db="site3local"			#Site Database
-dbUser="mysiteuser"
-dbPwd="fsoft31"
-siteName="Site3 File Based Local"
-siteMail="francis@choughs.net"
-acName="mysiteuser"
-acPwd="n4YodGm4CSmyK3SGNQjr"
-acMail="francis@choughs.net"
+workingFolder="/home/user/bin/drupalInstall"
+siteFolder="mysiteName"	#Location of site
+apacheRoot="/path/to/apacheRoot"
+apacheUser="apacheUser"
+apacheGroup="apacheGroup"
+db="dbName"			#Site Database
+dbUser="dbUserser"
+dbPwd="dbPasswd"
+siteName="MySite Name"
+siteMail="me@mysite.com"
+acName="mysiteUser"
+acPwd="mysitePassword"
+acMail="me@mysite.com"
 privatePath="$apacheRoot/$siteFolder/sites/default/files/private"
-trustedHosts="^site3\\.local$"
+trustedHosts="^mysite\\.com$"
 
